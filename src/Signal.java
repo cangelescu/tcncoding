d5e5b4c72f0c58ce38954b52a873a4e5805ef009
Oleksandr Natalenko <pfactum@gmail.com>
2010-05-04 22:03:20 +0300

    implement javadoc for signal class

--- src/Signal.java
+++ src/Signal.java
@@ -18,43 +18,77 @@
 
 import flanagan.integration.IntegralFunction;
 
-public class Signal implements IntegralFunction {
+/**
+ *
+ * @author post-factum
+ */
+public class Signal implements IntegralFunction
+{
     double frequency;
     double amplitude;
     double phase;
     double maxValue;
     double xStart, xEnd;
 
+    /**
+     * Returns f(x) for current signal
+     * @param x time variable, s
+     * @return
+     */
     public double function(double x)
     {
 	return 0;
     }
 
+    /**
+     * Returns signal frequency, Hz
+     * @return
+     */
     public double getFrequency()
     {
 	return this.frequency;
     }
 
+    /**
+     * Returns signal amplitude, V
+     * @return
+     */
     public double getAmplitude()
     {
 	return this.amplitude;
     }
 
+    /**
+     * Returns signal phase, rad
+     * @return
+     */
     public double getPhase()
     {
 	return this.phase;
     }
 
+    /**
+     * Returns signal maximum value, V
+     * @return
+     */
     public double getMaxValue()
     {
 	return this.maxValue;
     }
 
+    /**
+     * Returns signal start point, s
+     * @return
+     */
     public double getStart()
     {
 	return this.xStart;
     }
 
+    /**
+     * Returns signal end point, s
+     * @return
+     */
     public double getEnd()
     {
 	return this.xEnd;
