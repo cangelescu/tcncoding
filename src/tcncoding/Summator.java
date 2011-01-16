15a7f15005dc841fbad07ff2f5052a9580af0670
Oleksandr Natalenko <pfactum@gmail.com>
2011-01-17 00:15:43 +0200

    make summator much more scalable

--- src/tcncoding/Summator.java
+++ src/tcncoding/Summator.java
@@ -32,7 +32,7 @@ public class Summator
 {
     private List<List<DigitalSignal>> sequence0;
     private List<List<DigitalSignal>> sequence1;
-    private List<List<DigitalSignal>> sumResult = new ArrayList<List<DigitalSignal>>();
+    private List<List<DigitalSignal>> result = new ArrayList<List<DigitalSignal>>();
 
     /**
      * Creates summator of two digital functions
@@ -50,25 +50,104 @@ public class Summator
      */
     public void doSumming()
     {
-	sumResult.clear();
+	result.clear();
+
+        //creates signals map (sequence0)
+        List<Integer> signalsMap0 = new ArrayList<Integer>();
+        for (List<DigitalSignal> clms: sequence0)
+            signalsMap0.add(clms.size());
+
+        //creates flat list of signals (sequence0)
+        List<DigitalSignal> flatList0 = new ArrayList<DigitalSignal>();
+        for (List<DigitalSignal> clms: sequence0)
+            for (DigitalSignal cms: clms)
+                flatList0.add(cms);
+
+        //creates workers map, each worker gets its amount of pieces to process
+        int workers = Runtime.getRuntime().availableProcessors();
+        int tickets = flatList0.size();
+        int basePiece = tickets / workers;
+        if (basePiece < 1)
+            basePiece = 1;
+        List<Integer> workersMap = new ArrayList<Integer>();
+        for (int i = 0; i < workers - 1; i++)
+        {
+            workersMap.add(basePiece);
+            tickets -= basePiece;
+        }
+        //but the last worker may get more
+        workersMap.add(tickets);
+
+        //create signals map according to workers map (sequence0)
+        List<List<DigitalSignal>> newSignals0 = new ArrayList<List<DigitalSignal>>();
+        int index = 0;
+        for (Integer ci: workersMap)
+        {
+            List<DigitalSignal> newBlock = new ArrayList<DigitalSignal>();
+            for (int i = index; i < index + ci; i++)
+                newBlock.add(flatList0.get(i));
+            newSignals0.add(newBlock);
+            index += ci;
+        }
+
+        //creates signals map (sequence1)
+        List<Integer> signalsMap1 = new ArrayList<Integer>();
+        for (List<DigitalSignal> clms: sequence1)
+            signalsMap1.add(clms.size());
+
+        //creates flat list of signals (sequence1)
+        List<DigitalSignal> flatList1 = new ArrayList<DigitalSignal>();
+        for (List<DigitalSignal> clms: sequence1)
+            for (DigitalSignal cms: clms)
+                flatList1.add(cms);
+
+        //create signals map according to workers map (sequence1)
+        List<List<DigitalSignal>> newSignals1 = new ArrayList<List<DigitalSignal>>();
+        index = 0;
+        for (Integer ci: workersMap)
+        {
+            List<DigitalSignal> newBlock = new ArrayList<DigitalSignal>();
+            for (int i = index; i < index + ci; i++)
+                newBlock.add(flatList1.get(i));
+            newSignals1.add(newBlock);
+            index += ci;
+        }
 
         //creates workers stack
         List<Future<List<DigitalSignal>>> workersStack = new ArrayList<Future<List<DigitalSignal>>>();
-        ExecutorService es = Executors.newFixedThreadPool(sequence0.size());
-        for (int i = 0; i < sequence0.size(); i++)
-            workersStack.add(es.submit(new SummatorWorker(sequence0.get(i), sequence1.get(i))));
+        ExecutorService es = Executors.newFixedThreadPool(workers);
+        for (int i = 0; i < newSignals0.size(); i++)
+            workersStack.add(es.submit(new SummatorWorker(newSignals0.get(i), newSignals1.get(i))));
 
 
         //retrieves results
+        List<List<DigitalSignal>> rawResult = new ArrayList<List<DigitalSignal>>();
 	for (Future<List<DigitalSignal>> cflds: workersStack)
         {
             try {
-                sumResult.add(cflds.get());
+                rawResult.add(cflds.get());
             } catch (Exception ex) {
                 System.err.println(ex.getLocalizedMessage());
             }
         }
         es.shutdown();
+
+        //creates flat list from raw results
+        List<DigitalSignal> flatRawList = new ArrayList<DigitalSignal>();
+        for (List<DigitalSignal> clms: rawResult)
+            for (DigitalSignal cms: clms)
+                flatRawList.add(cms);
+
+        //repacks raw results according to signals map
+        index = 0;
+        for (Integer ci: signalsMap0)
+        {
+            List<DigitalSignal> newBlock = new ArrayList<DigitalSignal>();
+            for (int i = index; i < index + ci; i++)
+                newBlock.add(flatRawList.get(i));
+            index += ci;
+            result.add(newBlock);
+        }
     }
 
     /**
@@ -77,6 +156,6 @@ public class Summator
      */
     public List<List<DigitalSignal>> getSum()
     {
-	return sumResult;
+	return result;
     }
 }
